0b6d18dd71b183a9bab2da07d013efaccdd31b04
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-08 18:56:34 -0500

    add test: for #add_bid

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,8 +4,16 @@ require './lib/item'
 
 RSpec.describe Auction do
   let(:auction) {Auction.new}
+  
   let(:item1) { Item.new('Chalkware Piggy Bank')}
   let(:item2) { Item.new('Bamboo Picture Frame')}
+  let(:item3) { Item.new('Homemade Choclote Chip Cookies')}
+  let(:item4) { Item.new('2 Days Dogsitting')}
+  let(:item5) { Item.new('Forever Stamps')}
+
+  let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50')}
+  let(:attendee2) { Attendee.new(name: 'Bob', budget: '$75')}
+  let(:attendee3) { Attendee.new(name: 'Mike', budget: '$100')}
 
   
   describe '#initialize' do
@@ -19,7 +27,7 @@ RSpec.describe Auction do
   end
 
   describe '#add_item' do
-    it 'adds item to items' do
+    it 'adds item to items array' do
       auction.add_item(item1)
       auction.add_item(item2)
 
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,6 +2,14 @@ require './lib/item'
 
 RSpec.describe Item do
   let(:item1) { Item.new('Chalkware Piggy Bank')}
+  let(:item2) { Item.new('Bamboo Picture Frame')}
+  let(:item3) { Item.new('Homemade Choclote Chip Cookies')}
+  let(:item4) { Item.new('2 Days Dogsitting')}
+  let(:item5) { Item.new('Forever Stamps')}
+
+
+  let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50')}
+  let(:attendee2) { Attendee.new(name: 'Bob', budget: '$75')}
   
   describe '#initialize' do
     it 'exists' do
@@ -11,6 +19,15 @@ RSpec.describe Item do
     it 'has attributes' do
       expect(item1.name).to eq('Chalkware Piggy Bank')
       expect(item1.bids).to eq({})
+    end
+  end
+
+  describe '#add_bid' do
+    it 'can add bid to items' do
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expect(item1.bids)to eq({attendee2 => 20, attendee1 => 22})
     end
   end
 end
