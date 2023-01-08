bdc8fd956b50c32242d10b5943bba5f5490c03f5
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-08 18:00:12 -0500

    add test: for auction

---
+++ lib/auction.rb
@@ -0,0 +1,7 @@
+class Auction
+  attr_reader
+
+  def initialize()
+    
+  end
+end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,3 +1,16 @@
 require './lib/auction'
 require './lib/attendee'
 require './lib/item'
+
+RSpec.describe Auction do
+  let(:auction) {Auction.new}
+  describe '#initialize' do
+    it 'exists' do
+      expect(auction).to be_an_instance_of(Auction)
+    end
+
+    it 'has attributes' do
+      expect(attendee.items).to eq([])
+    end
+  end
+end
