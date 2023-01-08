bd87f7ba7b563cfc955006b8c486a9eada9623df
Boston Lowrey <bostonlowrey@gmail.com>
2023-01-08 18:06:23 -0500

    feat: can #add_item to items[]

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,6 +4,8 @@ require './lib/item'
 
 RSpec.describe Auction do
   let(:auction) {Auction.new}
+  let(:item1) { Item.new('Chalkware Piggy Bank')}
+  let(:item2) { Item.new('Bamboo Picture Frame')}
   describe '#initialize' do
     it 'exists' do
       expect(auction).to be_an_instance_of(Auction)
@@ -11,6 +13,15 @@ RSpec.describe Auction do
 
     it 'has attributes' do
       expect(auction.items).to eq([])
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds item to items' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq([item1, item2])
     end
   end
 end
